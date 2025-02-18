9f9dacb4f260104c1cbd41782921e507dc586861
AndrewKozyrev <mr.andrey.kozyrev@gmail.com>
2025-02-18 03:53:08 +0300

    fix

--- src/main/java/com/example/consumer/service/KafkaConsumerService.java
+++ src/main/java/com/example/consumer/service/KafkaConsumerService.java
@@ -3,12 +3,13 @@ package com.example.consumer.service;
 import com.example.consumer.dto.OrderDTO;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class KafkaConsumerService {
 
     private final ObjectMapper objectMapper;
@@ -21,10 +22,9 @@ public class KafkaConsumerService {
     public void listen(ConsumerRecord<String, String> record) {
         try {
             OrderDTO order = objectMapper.readValue(record.value(), OrderDTO.class);
-            System.out.println("Получен заказ: " + order);
+            log.info("Получен заказ: " + order);
         } catch (JsonProcessingException e) {
-            System.err.println("Ошибка десериализации: " + e.getMessage());
+            log.error(">> ошибка парсинга заказа.", e);
         }
-        System.out.println("Получено сообщение: " + record.value());
     }
 }
